8148d35325252bef0202d50ccd406ae660ace96f
Marcel Klein <33481835+Kleinemann@users.noreply.github.com>
2025-02-26 07:38:23 +0100

    start new chunk funktions

--- test/Map.cs
+++ test/Map.cs
@@ -21,17 +21,63 @@ public partial class Map : Node3D
 
     public Vector2I off;
 
+    public static Map _Map;
 
-    public Dictionary<Vector2I, Chunk> Chunks = new Dictionary<Vector2I, Chunk>();
-
+    public Array<Chunk> Chunks = new Array<Chunk>();
+    public Chunk CurrentChunk;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
+        _Map = this;
+
+        CurrentChunk = GetChunk(Player.Position);
 	}
+
+    void UpdateMap()
+    {
+
+    }
+
+    Chunk GetChunk(Vector3 pos)
+    {
+        Vector3 cPos = ChunkPos(pos);
+
+        foreach(Chunk c in Chunks)
+        {
+            if (c.Position == cPos)
+                return c;
+        }
+
+        return CreateChunk(cPos);
+    }
+
+    Chunk CreateChunk(Vector3 pos)
+    {
+        PackedScene scene = (PackedScene)ResourceLoader.Load("res://test/Chunk.tscn");
+        Chunk chunk = scene.Instantiate<Chunk>();
+
+        chunk.Position = ChunkPos(pos);
+        chunk.Scale = new Vector3(ChunkWidth, 1, ChunkHeigth);
+
+        Chunks.Add(chunk);
+        AddChild(chunk);
+
+        return chunk;
+    }
+
+    Vector3 ChunkPos(Vector3 pos) 
+    {
+        float x = (int)pos.X / ChunkWidth;
+        float z = (int)pos.Z / ChunkHeigth;
+        Vector3 cPos = new Vector3(x, 0, z);
+
+        return cPos;
+    }
 
 	void UpdateChunks()
 	{
+        /*
         foreach (Chunk chunk in Chunks.Values)
             chunk.ChunkState = Chunk.ChunkStateEnum.REMOVE;
 
@@ -61,16 +107,19 @@ public partial class Map : Node3D
             Chunks.Remove(c.Key);
             c.Value.QueueFree();
         }
+        */
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+        /*
         Vector2I o = new Vector2I(((int)Player.Position.X), ((int)Player.Position.Z));
         if (o != off)
         {
             off = o;
             UpdateChunks();
         }
+        */
     }
 }
