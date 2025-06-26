db2a919e01cc3d1ae09308398a4b3c43c4622a00
Marcel Klein <33481835+Kleinemann@users.noreply.github.com>
2025-06-26 08:01:17 +0200

    new test

--- World.cs
+++ World.cs
@@ -7,7 +7,7 @@ public enum TileType
 {
     Water,
     Grass,
-    Dirt
+    Sand
 }
 
 [Tool]
@@ -18,21 +18,38 @@ public partial class World : Node2D
 
     readonly Dictionary<Tuple<TileType, TileType, TileType, TileType>, Vector2I> neighboursToAtlasCoord = new() {
         {new (Grass, Grass, Grass, Grass), new Vector2I(2, 1)}, // All corners
-        {new (Dirt, Dirt, Dirt, Grass), new Vector2I(1, 3)}, // Outer bottom-right corner
-        {new (Dirt, Dirt, Grass, Dirt), new Vector2I(0, 0)}, // Outer bottom-left corner
-        {new (Dirt, Grass, Dirt, Dirt), new Vector2I(0, 2)}, // Outer top-right corner
-        {new (Grass, Dirt, Dirt, Dirt), new Vector2I(3, 3)}, // Outer top-left corner
-        {new (Dirt, Grass, Dirt, Grass), new Vector2I(1, 0)}, // Right edge
-        {new (Grass, Dirt, Grass, Dirt), new Vector2I(3, 2)}, // Left edge
-        {new (Dirt, Dirt, Grass, Grass), new Vector2I(3, 0)}, // Bottom edge
-        {new (Grass, Grass, Dirt, Dirt), new Vector2I(1, 2)}, // Top edge
-        {new (Dirt, Grass, Grass, Grass), new Vector2I(1, 1)}, // Inner bottom-right corner
-        {new (Grass, Dirt, Grass, Grass), new Vector2I(2, 0)}, // Inner bottom-left corner
-        {new (Grass, Grass, Dirt, Grass), new Vector2I(2, 2)}, // Inner top-right corner
-        {new (Grass, Grass, Grass, Dirt), new Vector2I(3, 1)}, // Inner top-left corner
-        {new (Dirt, Grass, Grass, Dirt), new Vector2I(2, 3)}, // Bottom-left top-right corners
-        {new (Grass, Dirt, Dirt, Grass), new Vector2I(0, 1)}, // Top-left down-right corners
-		{new (Dirt, Dirt, Dirt, Dirt), new Vector2I(0, 3)}, // No corners
+        {new (Sand, Sand, Sand, Grass), new Vector2I(1, 3)}, // Outer bottom-right corner
+        {new (Sand, Sand, Grass, Sand), new Vector2I(0, 0)}, // Outer bottom-left corner
+        {new (Sand, Grass, Sand, Sand), new Vector2I(0, 2)}, // Outer top-right corner
+        {new (Grass, Sand, Sand, Sand), new Vector2I(3, 3)}, // Outer top-left corner
+        {new (Sand, Grass, Sand, Grass), new Vector2I(1, 0)}, // Right edge
+        {new (Grass, Sand, Grass, Sand), new Vector2I(3, 2)}, // Left edge
+        {new (Sand, Sand, Grass, Grass), new Vector2I(3, 0)}, // Bottom edge
+        {new (Grass, Grass, Sand, Sand), new Vector2I(1, 2)}, // Top edge
+        {new (Sand, Grass, Grass, Grass), new Vector2I(1, 1)}, // Inner bottom-right corner
+        {new (Grass, Sand, Grass, Grass), new Vector2I(2, 0)}, // Inner bottom-left corner
+        {new (Grass, Grass, Sand, Grass), new Vector2I(2, 2)}, // Inner top-right corner
+        {new (Grass, Grass, Grass, Sand), new Vector2I(3, 1)}, // Inner top-left corner
+        {new (Sand, Grass, Grass, Sand), new Vector2I(2, 3)}, // Bottom-left top-right corners
+        {new (Grass, Sand, Sand, Grass), new Vector2I(0, 1)}, // Top-left down-right corners
+		{new (Sand, Sand, Sand, Sand), new Vector2I(0, 3)}, // No corners
+
+        //{new (Sand, Sand, Sand, Sand), new Vector2I(2, 1)}, // All corners
+        {new (Water, Water, Water, Sand), new Vector2I(1, 3)}, // Outer bottom-right corner
+        {new (Water, Water, Sand, Water), new Vector2I(0, 0)}, // Outer bottom-left corner
+        {new (Water, Sand, Water, Water), new Vector2I(0, 2)}, // Outer top-right corner
+        {new (Sand, Water, Water, Water), new Vector2I(3, 3)}, // Outer top-left corner
+        {new (Water, Sand, Water, Sand), new Vector2I(1, 0)}, // Right edge
+        {new (Sand, Water, Sand, Water), new Vector2I(3, 2)}, // Left edge
+        {new (Water, Water, Sand, Sand), new Vector2I(3, 0)}, // Bottom edge
+        {new (Sand, Sand, Water, Water), new Vector2I(1, 2)}, // Top edge
+        {new (Water, Sand, Sand, Sand), new Vector2I(1, 1)}, // Inner bottom-right corner
+        {new (Sand, Water, Sand, Sand), new Vector2I(2, 0)}, // Inner bottom-left corner
+        {new (Sand, Sand, Water, Sand), new Vector2I(2, 2)}, // Inner top-right corner
+        {new (Sand, Sand, Sand, Water), new Vector2I(3, 1)}, // Inner top-left corner
+        {new (Water, Sand, Sand, Water), new Vector2I(2, 3)}, // Bottom-left top-right corners
+        {new (Sand, Water, Water, Sand), new Vector2I(0, 1)}, // Top-left down-right corners
+		{new (Water, Water, Water, Water), new Vector2I(0, 3)}, // No corners
     };
 
 
@@ -74,7 +91,7 @@ public partial class World : Node2D
 
 
     TileMapLayer _water = null;
-    TileMapLayer Water
+    TileMapLayer WaterLayer
     {
         get
         {
@@ -126,7 +143,7 @@ public partial class World : Node2D
 
     void RefreshValues()
     {
-        if(Noise == null || Ground == null || Water == null)
+        if(Noise == null || Ground == null || WaterLayer == null)
         {
             GD.PrintErr("TileMapLayer not found, cannot refresh values.");
             return;
@@ -144,7 +161,7 @@ public partial class World : Node2D
         {
             for (int y = -1; y <= ChunkHeigth+1; y++)
             {
-                Water.SetCell(new Vector2I(x, y), 0, new Vector2I(2, 1));
+                WaterLayer.SetCell(new Vector2I(x, y), 0, new Vector2I(2, 1));
                 GroundDisplay.EraseCell(new Vector2I(x, y));
             }
         }
@@ -184,7 +201,7 @@ public partial class World : Node2D
             if (tileType == TileType.Grass)
                 atlasPos.Y += 8; // Adjust for grass tiles in the atlas
 
-            if (tileType == TileType.Dirt)
+            if (tileType == TileType.Sand)
                 atlasPos.Y += 4; // Adjust for dirt tiles in the atlas
 
             GroundDisplay.SetCell(newPos, 0, atlasPos);
@@ -213,19 +230,12 @@ public partial class World : Node2D
     {
         float value = GetNoise(pos);
 
-        GD.Print(value);
-
-        if (value > 0)
-            return TileType.Grass;
-        else
-            return TileType.Dirt;
-        /*
         if (value > 0.2)
             return TileType.Grass;
 
         if (value > 0.0)
-            return TileType.Dirt;
-        */
+            return TileType.Sand;
+
         return TileType.Water;
     }
 
