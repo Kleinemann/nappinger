a3a0cb0892f7bd7c0130cccaafbc1830d35eda38
Marcel Klein <33481835+Kleinemann@users.noreply.github.com>
2026-04-01 16:22:35 +0200

    create deroyable tree

---
+++ scripts/BreakableObject.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class BreakableObject : StaticBody2D
+{
+    [Export] public AnimatedSprite2D Sprite;
+    [Export] public AnimatedSprite2D SpriteShadow;
+    [Export] public CollisionShape2D CollisionShape;
+
+    public int Healt = 10;
+    public int MaxHealt =10;
+
+    public override void _Input(InputEvent @event)
+    {
+        //Focus / defocus Player
+        if (@event.IsActionPressed("kill"))
+        {
+            Healt--;
+            if(Healt < 0)
+                Healt = 0;
+            UpdateAnimation();
+        }
+    }
+
+    public void UpdateAnimation()
+    {
+          float healt = (Healt * 100 / MaxHealt);
+
+        if (healt == 0) Sprite.Play("3");
+        else if (healt < 40) Sprite.Play("2");
+        else if (healt < 70) Sprite.Play("1");
+        else Sprite.Play("0");
+
+        if (healt == 0) SpriteShadow.Play("3");
+        else if (healt < 40) SpriteShadow.Play("2");
+        else if (healt < 70) SpriteShadow.Play("1");
+        else SpriteShadow.Play("0");
+
+    }
+}
+
